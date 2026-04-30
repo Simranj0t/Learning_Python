409d1ca937bd1e404243999e0d7c86db2d664ce4
Simranj0t <simranjotsinghkohli@gmail.com>
2026-04-30 18:59:22 +0530

    Initial commit

---
+++ Variables.py
@@ -0,0 +1,34 @@
+# Create a variable named name and assign it your full name as a string. Then print the variable
+name = "Simranjot Singh Kohli"
+print(name)
+
+# Assign the values 10, 20, and 30 to variables a, b, and c in a single line. Then print their sum.
+a=10
+b=20
+c=30
+print("Sum of a, b, c is :",a+b+c)
+
+# Given two variables:
+# x = 5
+# y = 10
+# Swap their values without using a third variable, and print the new values of x and y
+x=5
+y=10
+
+x,y = y,x
+print("x =", x)
+print("y =", y)
+print("x =", x, "\n""y =", y)
+print(f"x = {x} \ny = {y}")
+
+# Ask the user to enter their favorite color using input(), assign it to a variable called color, and print:
+# Your favorite color is <color>.
+color = input("Enter your favorite color:")
+print("Your favorite color is :",color)
+
+# Assign a value to a variable radius. Calculate the area of a circle using the formula:
+# area = 3.14 * radius * radius
+# Print the result with a descriptive message.
+radius = int(input("Enter the Radius whose Area you want to find out :"))
+area = 3.14 * radius * radius
+print("The area of the circle is :",area)
