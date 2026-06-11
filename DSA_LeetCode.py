3e6b2c75636c3e6f454b1b6bc7f8a0ea4bdfa2f5
Simranj0t <simranjotsinghkohli@gmail.com>
2026-06-11 11:44:44 +0530

    Created New DSA_Leetcode.Py File and updated the code

---
+++ DSA_LeetCode.py
@@ -0,0 +1,18 @@
+# Maximum Total Subarray Value 
+
+nums = [2,5,8]
+k = 2
+
+max_numb = nums[0]
+min_numb = nums[0]
+
+for i in nums:
+    if i > max_numb:
+        max_numb = i
+    if i < min_numb:
+        min_numb = i 
+    value = (max_numb - min_numb )*k
+
+print(value)
+
+###################################################################################
