b0190c8159ad39cef997141fccc2f68cb44a932d
Simranj0t <simranjotsinghkohli@gmail.com>
2026-06-23 20:03:59 +0530

    Adding to the File Handing File

--- File_Handling.py
+++ File_Handling.py
@@ -4,4 +4,23 @@ file = open("demo.txt","r")
 data = file.read()
 print(data)
 print(type(data))
-f.close
+file.close()
+
+# To read the file line by line 
+
+file = open("demo.txt","r")
+line1 = file.readline()
+print(line1)
+
+line2 = file.readline()
+print(line2)
+file.close()
+
+# or this command can also work as 
+
+with open("demo.txt", "r") as file:
+    line1 = file.readline()
+    line2 = file.readline()
+
+print(line1)
+print(line2)
